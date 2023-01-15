7c068eaa79baf22337a9497889fadd1c8de33ab2
Flavio Auciello <flavioauciello@nebulab.com>
2023-01-15 01:46:15 +0100

    Adjust mix.exs (it was part of an umbrella app)

--- mix.exs
+++ mix.exs
@@ -5,10 +5,6 @@ defmodule Banshee.MixProject do
     [
       app: :banshee,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
