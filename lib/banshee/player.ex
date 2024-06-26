846ce8d4b2010d08167d3bc3a92fe9ca01dd6c21
Invisigoth <invisigoth@MBPdiInvisigoth.lan>
2024-06-26 12:09:10 +0200

    Refactor via Player.Runner: playand play with wait

---
+++ lib/banshee/player.ex
@@ -0,0 +1,49 @@
+defmodule Banshee.Player do
+  use GenServer
+
+  @initial_state %{ports: %{}}
+
+  def start_link do
+    GenServer.start_link(__MODULE__, @initial_state, name: __MODULE__)
+  end
+
+  @impl true
+  def init(state), do: {:ok, state}
+
+  def play(file) do
+    GenServer.cast(__MODULE__, {:play, file})
+  end
+
+  @play_ended_message :play_ended
+  def play(file, :wait) do
+    GenServer.call(__MODULE__, {:play, file})
+    receive do
+      @play_ended_message -> :ok
+    end
+  end
+
+  @impl true
+  def handle_call({:play, file}, caller, state) do
+    port = Port.open({:spawn, "#{player_executable()} #{file}"}, [])
+    Port.monitor(port)
+    {:reply, :ok, %{state | ports: Map.put(state.ports, port, caller)}}
+  end
+
+  @impl true
+  def handle_cast({:play, file}, state) do
+    port = Port.open({:spawn, "#{player_executable()} #{file}"}, [])
+    {:noreply, %{state | ports: Map.put(state.ports, port, nil)}}
+  end
+
+  @impl true
+  def handle_info({:DOWN, _ref, :port, down_port, _reason}, state) do
+    {caller, ports} = Map.pop(state.ports, down_port)
+    with {pid, _ref} <- caller, do: send(pid, :play_ended)
+    {:noreply, %{state | ports: ports}}
+  end
+
+  @default_player_executable "afplay"
+  defp player_executable do
+    Application.get_env(:banshee, :player_executable, @default_player_executable)
+  end
+end
