86e50afb4fd7f781bdb7bdd45eb31eaf560e194a
MinasMazar <5578468+MinasMazar@users.noreply.github.com>
2024-08-19 19:27:22 +0200

    Parameterize executables via ENVs

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,5 @@ import Config
 
 config :banshee,
   alarm_file: Path.expand("../../assets/banshee_scream.wav", __ENV__.file),
-  player_executable: "afplay",
-  tts_executable: "espeak"
+  player_executable: System.get_env("BANSHEE_PLAYER_EXECUTABLE", "afplay"),
+  tts_executable: System.get_env("BANSHEE_TTS_EXECUTABLE", "espeak")
