b869436a56ed0d0b8861c7968877f90efbed0fd1
MinasMazar <5578468+MinasMazar@users.noreply.github.com>
2024-08-19 20:04:08 +0200

    Rearrange app configuration

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,5 @@
 import Config
 
 config :banshee,
-  alarm_file: Path.expand("../../assets/banshee_scream.wav", __ENV__.file),
   player_executable: System.get_env("BANSHEE_PLAYER_EXECUTABLE", "afplay"),
   tts_executable: System.get_env("BANSHEE_TTS_EXECUTABLE", "espeak")
--- lib/banshee.ex
+++ lib/banshee.ex
@@ -6,8 +6,9 @@ defmodule Banshee do
   defdelegate play(file, mode), to: Banshee.Server
   defdelegate stop(), to: Banshee.Server
 
+  @default_alarm_file Path.expand("../../assets/banshee_scream.wav", __ENV__.file)
   def scream! do
-    Application.get_env(:banshee, :alarm_file) |> play()
+    Application.get_env(:banshee, :alarm_file, @default_alarm_file) |> play()
   end
 
   @default_player_executable "afplay"
